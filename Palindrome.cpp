78cfa1ec1ba6c5313e0e5814311a9613728d7ae4
Shubham Karmveer <s.karmveer1201@gmail.com>
2022-08-03 22:52:56 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,11 +1,67 @@
-#include <iostream>
-#include "Stack.h"
+#include<iostream>
 using namespace std;
-bool is_palindrome(string text){
+
+//creating class;
+class Stack{
+
+    private:
+        int n, top;
+        char *arr;
+
+    public:
+        Stack(int size){
+            n=size-1;
+            arr=new char[n];
+            top=-1;
+        }
     
-    int len = text.length()/2;
-    Stack st(len);
+        bool isEmpty(){
+            if(top<=-1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        bool isFull(){
+            if(top>n-1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+            //push function
+        int push(char val) {
+            arr[++top]=val;
+        }
+            
+            //pop function
+        char pop() {
+            char a = arr[top];
+            top--;
+            return a;
+        }
+
+            //display function
+        void display() {
+            cout<<"Stack elements are:\t";
+            for(int i=top; i>=0; i--)
+                cout<<arr[i]<<" ";
+                cout<<endl;
+        }
+            
+        char TopElement(){
+            char val= pop();
+            push(val);
+            return val;
+        }        
+};
+
+bool is_palindrome(string text){
     int i;
+    int len=text.length()/2;
 
     for(i=0; i=len; i++){
         st.push (text[i]);
@@ -35,6 +91,10 @@ int main(){
     string word;
     cout<<"Enter the word"<<endl;
     cin>>word;
+    
+    int len = word.length()/2;
+    Stack st(len);
+
     bool res = is_palindrome(word);
     if(res==true){
         cout<<"The word is palindrome"<<endl;
