9b25fe1308980a1ea10ca9536b8c73b9e45f5727
Shubham Karmveer <s.karmveer1201@gmail.com>
2022-08-05 15:41:11 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,85 +1,27 @@
 #include<iostream>
+#include<conio.h>
+#include "Stacks.h"
 using namespace std;
 
-//creating class;
-class Stack{
+bool palindrome(string str)
+{
+    int i=0 ,size=str.size(), mid=(size/2);
+    Stacks St(mid);
 
-    private:
-        int n, top;
-        char *arr;
-
-    public:
-        Stack(int size){
-            n=size-1;
-            arr=new char[n];
-            top=-1;
-        }
-    
-        bool isEmpty(){
-            if(top<=-1){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        
-        bool isFull(){
-            if(top>n-1){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-            //push function
-        int push(char val) {
-            arr[++top]=val;
-        }
-            
-            //pop function
-        char pop() {
-            char a = arr[top];
-            top--;
-            return a;
-        }
-
-            //display function
-        void display() {
-            cout<<"Stack elements are:\t";
-            for(int i=top; i>=0; i--)
-                cout<<arr[i]<<" ";
-                cout<<endl;
-        }
-            
-        char TopElement(){
-            char val= pop();
-            push(val);
-            return val;
-        }        
-};
-
-bool is_palindrome(string text){
-    int i;
-    int len=text.length()/2;
-
-    for(i=0; i=len; i++){
-        st.push (text[i]);
+    for(i=0; i<mid; i++)
+    {
+        St.push (str[i]);
     }
 
-    if (text.length() % 2 != 0) {
+    if (size % 2 != 0)
+    {
         i++;
     }
 
-    while (text[i] != '\0')
+    while (str[i] != '\0')
     {
-        char ele = st.TopElement();
-        st.pop();
-
-    // If the characters differ then the
-    // given string is not a palindrome
-
-        if (ele != text[i])
+        char ele =  St.pop();
+        if (ele != str[i])
             return false;
         i++;
     }   
@@ -91,18 +33,15 @@ int main(){
     string word;
     cout<<"Enter the word"<<endl;
     cin>>word;
-    
-    int len = word.length()/2;
-    Stack st(len);
-
-    bool res = is_palindrome(word);
+    bool res = palindrome(word);
     if(res==true){
-        cout<<"The word is palindrome"<<endl;
+        cout<<"The word is palindrome."<<endl;
     }
     else{
-        cout<<"The word is not a palindrome"<<endl;
+        cout<<"The word is not a palindrome."<<endl;
     }
-    
+    getch();
     return 0;
 }
+
 
