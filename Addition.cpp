765087da1a91b77322ecf787a3cebbb4ef2d4dea
Shubham Karmveer <s.karmveer1201@gmail.com>
2022-08-19 17:52:08 +0530

    Update Addition.cpp

--- Addition.cpp
+++ Addition.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<conio.h>
-#include<string>
 #include "Stacks.h"
 using namespace std;
 
@@ -8,58 +7,49 @@ char addition(string num1, string num2)
 {
     int len1=num1.length();
     int len2=num2.length();
-    int carry=0,  sum=0, max;
-    
-    Stacks st1(len1);
-    Stacks st2(len2);
-    if (len1>=len2)
-        max=len1;
+    int carry=0,  sum=0;
+
+    //having equal length of both num
+    Stacks st1(len1), st2(len2);
+    if (len1< len2)
+        num1.insert(0, len2-len1, '0');
     else
-        max=len2;
-    Stacks Res(max+1);
+        num2.insert(0, len1-len2, '0');
+        
+    Stacks Res(num1.length()+1);
 
-    for (int i=0; i<len1; i++){
+    //inserting digits in array
+    for (int i=0; i<num1.length(); i++){
         st1.push(num1[i]);
+        st2.push(num2[i]);
     }
-    for (int i=0; i<len2; i++){
-        st2.push(num1[i]);
+    //adding while stack not get empty.
+    while(!(st1.isEmpty()))
+    {
+        int sum = int(st1.pop())-48 + int(st2.pop()) -48 + carry ;
+        Res.push(char(sum%10)+48);
+        carry=sum/10;   
     }
+    //inserting carry if occur
+    if (carry == 1)
+        Res.push('1');
 
-    while((st1.isEmpty()!=true) || (st2.isEmpty()!=true)){
-        sum=(static_cast<int>(st1.pop()))+(static_cast<int>(st2.pop()))+carry;
-        Res.push(static_cast<char>(sum%10));
-        carry=sum/10;
+    //displaying result o addition.
+    cout<<"Result of Addition:\t";
+    while(!Res.isEmpty()){
+        cout << Res.pop();
     }
-    while(st1.isEmpty()!=true){
-        sum=(static_cast<int>(st1.pop()))+carry;
-        Res.push(static_cast<char>(sum%10));
-        carry=sum/10;
-    }
-    while(st2.isEmpty()!=true){
-        sum=(static_cast<int>(st2.pop()))+carry;
-        Res.push(static_cast<char>(sum%10));
-        carry=sum/10;
-    }
-    while(carry>0){
-        Res.push(static_cast<char>(carry));
-        carry /= 10;
-    }
-
-    Res.display();
-
 }
 
-int main(){
-    long num1, num2;
+int main()
+{
+    string num1, num2;
     cout<<"Enter first number."<<endl;
     cin>>num1;
     cout<<"Enter second number."<<endl;
     cin>>num2;
 
-    string n1= to_string(num1);
-	string n2= to_string(num2);
-    addition(n1,n2);
+    addition(num1,num2);
     getch();
     return 0;
-
 }
