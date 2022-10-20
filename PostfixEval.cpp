22f542d2821b16cecf944d85b039a04f1c32c14f
Shubham Karmveer <s.karmveer1201@gmail.com>
2022-10-20 21:05:07 +0530

    Add files via upload

---
+++ PostfixEval.cpp
@@ -0,0 +1,46 @@
+// C++ program to evaluate value of a postfix expression
+#include <iostream>
+#include <conio.h>
+#include "Stacks.h"
+#include <cmath>
+using namespace std;
+
+int postfixEval(string term)
+{
+	int size=term.size();
+    Stacks opStk(size);
+
+	for (int i=0; term[i]; ++i)
+	{
+		// If the scanned character is an operand
+		// push it to the stack. else pop values and perform operation.
+		if (isdigit(term[i]))
+			opStk.push(term[i]-'0');
+
+		else
+		{
+			int val1 = opStk.pop();
+			int val2 = opStk.pop();
+			switch (term[i])
+			{
+            case '$': opStk.push(pow(val2,val1));break;
+			case '+': opStk.push(val2+val1); break;
+			case '-': opStk.push(val2-val1); break;
+			case '*': opStk.push(val2*val1); break;
+			case '/': opStk.push(val2/val1); break;
+			}
+		}
+	}
+	return opStk.pop();
+}
+
+// Driver program to test above functions
+int main()
+{
+    cout<<"Enter a Postfix expression: ";
+	string exp;
+    cin>>exp;
+	cout<<"postfix evaluation: "<< postfixEval(exp);
+    getch();
+	return 0;
+}
