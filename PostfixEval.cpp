84d62d7d76cc45c4a7227f9a7b62a658b7cccd01
Shubham Karmveer <s.karmveer1201@gmail.com>
2022-10-27 00:09:21 +0530

    Add files via upload

--- PostfixEval.cpp
+++ PostfixEval.cpp
@@ -9,21 +9,25 @@ int postfixEval(string term)
 {
 	int size=term.size();
     Stacks opStk(size);
-
-	for (int i=0; term[i]; ++i)
+	
+	for (int i=0; i<size; i++)
 	{
 		// If the scanned character is an operand
 		// push it to the stack. else pop values and perform operation.
+		cout<<term[i]<<" ";
 		if (isdigit(term[i]))
 			opStk.push(term[i]-'0');
 
 		else
 		{
 			int val1 = opStk.pop();
+			cout<<"val1 = "<<val1<<endl;
 			int val2 = opStk.pop();
+			cout<<"val2 = "<<val2<<endl;
 			switch (term[i])
 			{
-            case '$': opStk.push(pow(val2,val1));break;
+			
+			case '$': opStk.push(pow(val2,val1)); return pow(val2,val1); break;
 			case '+': opStk.push(val2+val1); break;
 			case '-': opStk.push(val2-val1); break;
 			case '*': opStk.push(val2*val1); break;
